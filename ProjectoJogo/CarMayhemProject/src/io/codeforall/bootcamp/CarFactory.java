9b508427c3a2c10729159ab3a8de18d314035899
Susana Eduardo <suzana_mrt@hotmail.com>
2024-09-17 12:03:01 +0100

    Changed the name from teste to CarMayhem

---
+++ ProjectoJogo/CarMayhemProject/src/io/codeforall/bootcamp/CarFactory.java
@@ -0,0 +1,12 @@
+package io.codeforall.bootcamp;
+
+public class CarFactory {
+
+
+    public static Car getNewCar(){
+        Car car = new Car();
+        return car;
+    }
+
+
+}
---
+++ ProjectoJogo/CarMayhemProject/src/io/codeforall/bootcamp/Main.java
@@ -0,0 +1,15 @@
+package io.codeforall.bootcamp;
+
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+
+
+
+
+        game.init();
+    }
+}
